5cca0da20a53dfb4c5103c65197dcaae40bc811c
paseosdentro <pmartinez2@uc.cl>
2020-07-06 19:55:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 <script> 
-function ventanaSecundaria (URL){ 
+function ventanaSecundaria (https://paseosdentro.github.io/paseosdentro/page2){ 
    window.open(https://paseosdentro.github.io/paseosdentro/page2,"ventana2","width=120,height=300,scrollbars=NO") 
 } 
 </script>
