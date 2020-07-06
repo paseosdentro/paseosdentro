06ff8eee4e40d3804701e5955870ce795b58ef06
paseosdentro <pmartinez2@uc.cl>
2020-07-06 19:53:15 -0400

    update index

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-$('.litetooltip-hotspot-wrapper .hotspot').each(function () { 
-    $(this).LiteTooltip({ title: $(this).find('.data-container').html() });
- });
+<script> 
+function ventanaSecundaria (URL){ 
+   window.open(https://paseosdentro.github.io/paseosdentro/page2,"ventana2","width=120,height=300,scrollbars=NO") 
+} 
+</script>
