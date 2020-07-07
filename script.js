4bf150c13bb76833b5c94f89cf353850abb7451d
paseosdentro <pmartinez2@uc.cl>
2020-07-07 01:24:39 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-<script> 
-function ventanaSecundaria (https://paseosdentro.github.io/paseosdentro/page2){ 
-   window.open(https://paseosdentro.github.io/paseosdentro/page2,"ventana2","width=120,height=300,scrollbars=NO") 
-} 
-</script>
+<a href="https://paseosdentro.github.io/paseosdentro/page2" target="popup1"
+onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page3" target="popup2"
+onClick="window.open(this.href, this.target, 'width=900px,height=600px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page4" target="popup3"
+onClick="window.open(this.href, this.target, 'width=600px,height=700px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page5" target="popup4"
+onClick="window.open(this.href, this.target, 'width=1500px,height=500px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page6" target="popup5"
+onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page7" target="popup6"
+onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page8" target="popup7"
+onClick="window.open(this.href, this.target, 'width=750px,height=600px'); return false;">
+observatorio
+</a>
+
+<a href="https://paseosdentro.github.io/paseosdentro/page9" target="popup8"
+onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
+observatorio
+</a>
