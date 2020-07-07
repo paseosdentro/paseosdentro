d2c575ef3ef34f2e7774a4ea57bab546bb841c6f
paseosdentro <pmartinez2@uc.cl>
2020-07-07 16:33:38 -0400

    update

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+
+
+
 <a href="https://paseosdentro.github.io/paseosdentro/page2" target="popup1"
 onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
 observatorio
 </a>
 
 <a href="https://paseosdentro.github.io/paseosdentro/page3" target="popup2"
-onClick="window.open(this.href, this.target, 'width=900px,height=600px'); return false;">
+onClick="window.open(this.href, this.target, 'width=900px,height=600px,resizable=no, left=700%, top=200%'); return false;">
 observatorio
 </a>
 
 <a href="https://paseosdentro.github.io/paseosdentro/page4" target="popup3"
-onClick="window.open(this.href, this.target, 'width=650px,height=600px'); return false;">
+onClick="window.open(this.href, this.target, 'width=650px,height=600px,top=600px'); return false;">
 observatorio
 </a>
 
@@ -19,12 +22,12 @@ observatorio
 </a>
 
 <a href="https://paseosdentro.github.io/paseosdentro/page6" target="popup5"
-onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
+onClick="window.open(this.href, this.target, 'width=900px,height=600px, resizable=no, top=40%px, left=60%'); return false;">
 observatorio
 </a>
 
 <a href="https://paseosdentro.github.io/paseosdentro/page7" target="popup6"
-onClick="window.open(this.href, this.target, 'width=1000px,height=700px'); return false;">
+onClick="window.open(this.href, this.target, 'width=1000px,height=700px, top=300px, left=300px'); return false;">
 observatorio
 </a>
 
@@ -34,6 +37,21 @@ observatorio
 </a>
 
 <a href="https://paseosdentro.github.io/paseosdentro/page9" target="popup8"
-onClick="window.open(this.href, this.target, 'width=300px,height=400px'); return false;">
+onClick="window.open(this.href, this.target, 'width=300px,height=400px,top=700px,left=140px'); return false;">
 observatorio
 </a>
+
+
+
+
+
+
+
+<script language=javascript> 
+function trazo1 (URL){ 
+   window.open(URL,"ventana1","width=120,height=300,scrollbars=NO") 
+} 
+
+trazo1("http://www.desarrolloweb.com"); 
+</script>
+
