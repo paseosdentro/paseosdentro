7617c2d2854fffb4552e08c253bda0e9768d943f
paseosdentro <pmartinez2@uc.cl>
2020-07-07 22:27:56 -0400

    update

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 
 
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page2" target="popup1"
+<a href="pages/page2.html" target="popup1"
 onClick="window.open(this.href, this.target, 'width=1600px,height=500px'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page3" target="popup2"
+<a href="pages/page3.html" target="popup2"
 onClick="window.open(this.href, this.target, 'width=900px,height=600px,resizable=no, left=700%, top=200%'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page4" target="popup3"
+<a href="pages/page4.html" target="popup3"
 onClick="window.open(this.href, this.target, 'width=650px,height=450px,top=600px'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page5" target="popup4"
+<a href="pages/page5.html" target="popup4"
 onClick="window.open(this.href, this.target, 'width=650px,height=400px'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page6" target="popup5"
+<a href="pages/page6.html" target="popup5"
 onClick="window.open(this.href, this.target, 'width=900px,height=600px, resizable=no, left=60px'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page7" target="popup6"
+<a href="pages/page7.html" target="popup6"
 onClick="window.open(this.href, this.target, 'width=1000px,height=700px, top=300px, left=300px'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page8" target="popup7"
+<a href="pages/page8.html" target="popup7"
 onClick="window.open(this.href, this.target, 'width=750px,height=600px,top=50px,left=50px'); return false;">
 observatorio
 </a>
 
-<a href="https://paseosdentro.github.io/paseosdentro/pages/page9" target="popup8"
+<a href="pages/page9.html" target="popup8"
 onClick="window.open(this.href, this.target, 'width=300px,height=400px,top=700px,left=140px'); return false;">
 observatorio
 </a>
